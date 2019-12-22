e3a429ae64c9332b4e2c8544ba6356db2f13ae04
Jon (BadQuanta) Sawyer <badquanta@gmail.com>
2019-12-21 19:33:36 -0800

    Basic cross compiler.

---
+++ CROSS-COMPLILER/getGNUSources.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+mkdir -p downloads
+wget -v https://ftp.gnu.org/gnu/binutils/binutils-2.33.1.tar.gz -O ./downloads/binutils.tar.gz
+wget -v https://ftp.gnu.org/gnu/gcc/gcc-9.2.0/gcc-9.2.0.tar.gz -O ./downloads/gcc.tar.gz
+
+
---
+++ CROSS-COMPLILER/preparation.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export PREFIX=`pwd`
+export TARGET=i386-elf
+export PATH="$PREFIX/bin:$PATH"
+
+echo $PREFIX
+echo $TARGET
+echo $PATH
